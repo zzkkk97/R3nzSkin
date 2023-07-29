dce842bc9a9f2f90b5a3f9e523407ffb9e9b2de8
xhr <65706755+xhr2@users.noreply.github.com>
2023-07-29 09:56:16 +0800

    Update memory.hpp with original content

--- R3nzSkin/memory.hpp
+++ R3nzSkin/memory.hpp
@@ -1,139 +1,76 @@
-#pragma once
-
-#include <Windows.h>
-#include <cstdint>
-#include <d3d9.h>
-#include <d3d11.h>
-
-#include "Offsets.hpp"
-
-#include "SDK/AIBaseCommon.hpp"
-#include "SDK/AIHero.hpp"
-#include "SDK/AITurret.hpp"
-#include "SDK/AIMinionClient.hpp"
-#include "SDK/ChampionManager.hpp"
-#include "SDK/GameClient.hpp"
-#include "SDK/ManagerTemplate.hpp"
-
-class offset_signature {
-public:
-	std::vector<std::string> pattern;
-	bool sub_base;
-	bool read;
-	bool relative;
-	std::int32_t additional;
-	std::uint64_t* offset;
-};
-
-class Memory {
-public:
-	void Search(bool gameClient = true);
-
-	std::uintptr_t base;
-	HWND window;
-
-	GameClient* client;
-	AIBaseCommon* localPlayer;
-	ManagerTemplate<AIHero>* heroList;
-	ManagerTemplate<AIMinionClient>* minionList;
-	ManagerTemplate<AITurret>* turretList;
-	ChampionManager* championManager;
-	
-	std::uintptr_t materialRegistry;
-	IDirect3DDevice9* d3dDevice;
-	IDXGISwapChain* swapChain;
-
-	using translateString_t = const char* (__fastcall*)(const char*);
-
-	translateString_t translateString;
-private:
-	void update(bool gameClient = true) noexcept;
-
-	std::vector<offset_signature> gameClientSig
-	{
-		{
-			{
-				"48 8B 05 ? ? ? ? 8B 58 0C"
-			}, true, false, true, 0, &offsets::global::GameClient
-		}
-	};
-
-	std::vector<offset_signature> sigs
-	{
-		{
-			{
-				"48 8B 3D ? ? ? ? 48 3B CF"
-			}, true, false, true, 0, &offsets::global::Player
-		},
-		{
-			{
-				"48 8B 05 ? ? ? ? 48 8B 78 08 8B 40 10 4C 8D 3C C7 49 3B FF"
-			}, true, false, true, 0, &offsets::global::ManagerTemplate_AIHero_
-		},
-		{
-			{
-				"48 8B 0D ? ? ? ? 48 69 D0 ? ? 00 00 48 8B 05"
-			}, true, false, true, 0, &offsets::global::ChampionManager
-		},
-		{
-			{
-				"48 89 0D ? ? ? ? 48 8D 05 ? ? ? ? 33 D2"
-			}, true, false, true, 0, &offsets::global::ManagerTemplate_AIMinionClient_
-		},
-		{
-			{
-				"48 89 0D ? ? ? ? 33 C9 48 8D 05 ? ? ? ? 4C 8D 05"
-			}, true, false, true, 0, &offsets::global::ManagerTemplate_AITurret_
-		},
-		{
-			{
-				"48 8B 0D ? ? ? ? FF 15 ? ? ? ? E8 ? ? ? ? 84 C0"
-			}, true, false, true, 0, &offsets::global::Riot__g_window
-		},
-		{
-			{
-				"48 8D 8B ? ? ? ? 48 89 44 24 ? C7 44 24"
-			}, false, true, false, 0, &offsets::AIBaseCommon::CharacterDataStack
-		},
-		{
-			{
-				"40 38 BB ? ? ? ? 0F 85 ? ? ? ? 66 C7 83 ? ? ? ? ? ? 0F 31 48 C1 E2 20 4C 8D 83 ? ? ? ? 48 0B C2 44 8B CF 48 89 44 24 ? 8B D7 41 BA ? ? ? ? 66 0F 1F 84 00"
-			}, false, true, false, 0, &offsets::AIBaseCommon::SkinId
-		},
-		{
-			{
-				"48 8B 8F ? ? 00 00 45 33 C0 8B D3 48 8B 01 FF 90 ? ? 00 00"
-			}, false, true, false, 0, &offsets::MaterialRegistry::D3DDevice
-		},
-		{
-			{
-				"48 8D BB ? ? ? ? C6 83 ? ? ? ? ? 0F 84"
-			}, false, true, false, 0, &offsets::MaterialRegistry::SwapChain
-		},
-		{
-			{
-				"E8 ? ? ? ? 48 8D 8D ? ? 00 00 E8 ? ? ? ? 48 85 C0"
-			}, true, false, false, 0, &offsets::functions::CharacterDataStack__Push
-		},
-		{
-			{
-				"88 54 24 10 53 55 56 57 41 54 41 55 41 56 41"
-			}, true, false, false, 0, &offsets::functions::CharacterDataStack__Update
-		},
-		{
-			{
-				"E8 ? ? ? ? 8B 57 54"
-			}, true, false, false, 0, &offsets::functions::Riot__Renderer__MaterialRegistry__GetSingletonPtr
-		},
-		{
-			{
-				"E8 ? ? ? ? 0F 57 DB 4C 8B C0 F3 0F 5A DE"
-			}, true, false, false, 0, &offsets::functions::translateString_UNSAFE_DONOTUSE
-		},
-		{
-			{
-				"E8 ? ? ? ? 4C 3B F8 0F 94 C0"
-			}, true, false, false, 0, &offsets::functions::GetGoldRedirectTarget
-		}
-	};
-};
+I3ByYWdtYSBvbmNlCgojaW5jbHVkZSA8V2luZG93cy5oPgojaW5jbHVkZSA8
+Y3N0ZGludD4KI2luY2x1ZGUgPGQzZDkuaD4KI2luY2x1ZGUgPGQzZDExLmg+
+CgojaW5jbHVkZSAiT2Zmc2V0cy5ocHAiCgojaW5jbHVkZSAiU0RLL0FJQmFz
+ZUNvbW1vbi5ocHAiCiNpbmNsdWRlICJTREsvQUlIZXJvLmhwcCIKI2luY2x1
+ZGUgIlNESy9BSVR1cnJldC5ocHAiCiNpbmNsdWRlICJTREsvQUlNaW5pb25D
+bGllbnQuaHBwIgojaW5jbHVkZSAiU0RLL0NoYW1waW9uTWFuYWdlci5ocHAi
+CiNpbmNsdWRlICJTREsvR2FtZUNsaWVudC5ocHAiCiNpbmNsdWRlICJTREsv
+TWFuYWdlclRlbXBsYXRlLmhwcCIKCmNsYXNzIG9mZnNldF9zaWduYXR1cmUg
+ewpwdWJsaWM6CglzdGQ6OnZlY3RvcjxzdGQ6OnN0cmluZz4gcGF0dGVybjsK
+CWJvb2wgc3ViX2Jhc2U7Cglib29sIHJlYWQ7Cglib29sIHJlbGF0aXZlOwoJ
+c3RkOjppbnQzMl90IGFkZGl0aW9uYWw7CglzdGQ6OnVpbnQ2NF90KiBvZmZz
+ZXQ7Cn07CgpjbGFzcyBNZW1vcnkgewpwdWJsaWM6Cgl2b2lkIFNlYXJjaChi
+b29sIGdhbWVDbGllbnQgPSB0cnVlKTsKCglzdGQ6OnVpbnRwdHJfdCBiYXNl
+OwoJSFdORCB3aW5kb3c7CgoJR2FtZUNsaWVudCogY2xpZW50OwoJQUlCYXNl
+Q29tbW9uKiBsb2NhbFBsYXllcjsKCU1hbmFnZXJUZW1wbGF0ZTxBSUhlcm8+
+KiBoZXJvTGlzdDsKCU1hbmFnZXJUZW1wbGF0ZTxBSU1pbmlvbkNsaWVudD4q
+IG1pbmlvbkxpc3Q7CglNYW5hZ2VyVGVtcGxhdGU8QUlUdXJyZXQ+KiB0dXJy
+ZXRMaXN0OwoJQ2hhbXBpb25NYW5hZ2VyKiBjaGFtcGlvbk1hbmFnZXI7CgkK
+CXN0ZDo6dWludHB0cl90IG1hdGVyaWFsUmVnaXN0cnk7CglJRGlyZWN0M0RE
+ZXZpY2U5KiBkM2REZXZpY2U7CglJRFhHSVN3YXBDaGFpbiogc3dhcENoYWlu
+OwoKCXVzaW5nIHRyYW5zbGF0ZVN0cmluZ190ID0gY29uc3QgY2hhciogKF9f
+ZmFzdGNhbGwqKShjb25zdCBjaGFyKik7CgoJdHJhbnNsYXRlU3RyaW5nX3Qg
+dHJhbnNsYXRlU3RyaW5nOwpwcml2YXRlOgoJdm9pZCB1cGRhdGUoYm9vbCBn
+YW1lQ2xpZW50ID0gdHJ1ZSkgbm9leGNlcHQ7CgoJc3RkOjp2ZWN0b3I8b2Zm
+c2V0X3NpZ25hdHVyZT4gZ2FtZUNsaWVudFNpZwoJewoJCXsKCQkJewoJCQkJ
+IjQ4IDhCIDA1ID8gPyA/ID8gOEIgNTggMEMiCgkJCX0sIHRydWUsIGZhbHNl
+LCB0cnVlLCAwLCAmb2Zmc2V0czo6Z2xvYmFsOjpHYW1lQ2xpZW50CgkJfQoJ
+fTsKCglzdGQ6OnZlY3RvcjxvZmZzZXRfc2lnbmF0dXJlPiBzaWdzCgl7CgkJ
+ewoJCQl7CgkJCQkiNDggOEIgM0QgPyA/ID8gPyA0OCAzQiBDRiIKCQkJfSwg
+dHJ1ZSwgZmFsc2UsIHRydWUsIDAsICZvZmZzZXRzOjpnbG9iYWw6OlBsYXll
+cgoJCX0sCgkJewoJCQl7CgkJCQkiNDggOEIgMDUgPyA/ID8gPyA0OCA4QiA3
+OCAwOCA4QiA0MCAxMCA0QyA4RCAzQyBDNyA0OSAzQiBGRiIKCQkJfSwgdHJ1
+ZSwgZmFsc2UsIHRydWUsIDAsICZvZmZzZXRzOjpnbG9iYWw6Ok1hbmFnZXJU
+ZW1wbGF0ZV9BSUhlcm9fCgkJfSwKCQl7CgkJCXsKCQkJCSI0OCA4QiAwRCA/
+ID8gPyA/IDQ4IDY5IEQwID8gPyAwMCAwMCA0OCA4QiAwNSIKCQkJfSwgdHJ1
+ZSwgZmFsc2UsIHRydWUsIDAsICZvZmZzZXRzOjpnbG9iYWw6OkNoYW1waW9u
+TWFuYWdlcgoJCX0sCgkJewoJCQl7CgkJCQkiNDggODkgMEQgPyA/ID8gPyA0
+OCA4RCAwNSA/ID8gPyA/IDMzIEQyIgoJCQl9LCB0cnVlLCBmYWxzZSwgdHJ1
+ZSwgMCwgJm9mZnNldHM6Omdsb2JhbDo6TWFuYWdlclRlbXBsYXRlX0FJTWlu
+aW9uQ2xpZW50XwoJCX0sCgkJewoJCQl7CgkJCQkiNDggODkgMEQgPyA/ID8g
+PyAzMyBDOSA0OCA4RCAwNSA/ID8gPyA/IDRDIDhEIDA1IgoJCQl9LCB0cnVl
+LCBmYWxzZSwgdHJ1ZSwgMCwgJm9mZnNldHM6Omdsb2JhbDo6TWFuYWdlclRl
+bXBsYXRlX0FJVHVycmV0XwoJCX0sCgkJewoJCQl7CgkJCQkiNDggOEIgMEQg
+PyA/ID8gPyBGRiAxNSA/ID8gPyA/IEU4ID8gPyA/ID8gODQgQzAiCgkJCX0s
+IHRydWUsIGZhbHNlLCB0cnVlLCAwLCAmb2Zmc2V0czo6Z2xvYmFsOjpSaW90
+X19nX3dpbmRvdwoJCX0sCgkJewoJCQl7CgkJCQkiNDggOEQgOEIgPyA/ID8g
+PyA0OCA4OSA0NCAyNCA/IEM3IDQ0IDI0IgoJCQl9LCBmYWxzZSwgdHJ1ZSwg
+ZmFsc2UsIDAsICZvZmZzZXRzOjpBSUJhc2VDb21tb246OkNoYXJhY3RlckRh
+dGFTdGFjawoJCX0sCgkJewoJCQl7CgkJCQkiNDAgMzggQkIgPyA/IDAwIDAw
+IDBGIDg1ID8gMDAgMDAgMDAgNjYgQzcgODMgPyA/IDAwIDAwIDAwIDA0IDBG
+IDMxIDQ4IEMxIEUyID8gNEMgOEQgODMgPyA/IDAwIDAwIDQ4IDBCIEMyIDQ0
+IDhCIENGIDQ4IDg5IDQ0ID8gMzggOEIgRDcgNDEgPyAwMSAwMCAwMCAwMCA2
+NiAwZiAxRiA4NCAwMCIKCQkJfSwgZmFsc2UsIHRydWUsIGZhbHNlLCAwLCAm
+b2Zmc2V0czo6QUlCYXNlQ29tbW9uOjpTa2luSWQKCQl9LAoJCXsKCQkJewoJ
+CQkJIjQ4IDhCIDhGID8gPyAwMCAwMCA0NSAzMyBDMCA4QiBEMyA0OCA4QiAw
+MSBGRiA5MCA/ID8gMDAgMDAiCgkJCX0sIGZhbHNlLCB0cnVlLCBmYWxzZSwg
+MCwgJm9mZnNldHM6Ok1hdGVyaWFsUmVnaXN0cnk6OkQzRERldmljZQoJCX0s
+CgkJewoJCQl7CgkJCQkiNDggOEQgQkIgPyA/ID8gPyBDNiA4MyA/ID8gPyA/
+ID8gMEYgODQiCgkJCX0sIGZhbHNlLCB0cnVlLCBmYWxzZSwgMCwgJm9mZnNl
+dHM6Ok1hdGVyaWFsUmVnaXN0cnk6OlN3YXBDaGFpbgoJCX0sCgkJewoJCQl7
+CgkJCQkiRTggPyA/ID8gPyA0OCA4RCA4RCA/ID8gMDAgMDAgRTggPyA/ID8g
+PyA0OCA4NSBDMCIKCQkJfSwgdHJ1ZSwgZmFsc2UsIGZhbHNlLCAwLCAmb2Zm
+c2V0czo6ZnVuY3Rpb25zOjpDaGFyYWN0ZXJEYXRhU3RhY2tfX1B1c2gKCQl9
+LAoJCXsKCQkJewoJCQkJIjg4IDU0IDI0IDEwIDUzIDU1IDU2IDU3IDQxIDU0
+IDQxIDU1IDQxIDU2IDQxIgoJCQl9LCB0cnVlLCBmYWxzZSwgZmFsc2UsIDAs
+ICZvZmZzZXRzOjpmdW5jdGlvbnM6OkNoYXJhY3RlckRhdGFTdGFja19fVXBk
+YXRlCgkJfSwKCQl7CgkJCXsKCQkJCSJFOCA/ID8gPyA/IDhCIDU3IDU0IgoJ
+CQl9LCB0cnVlLCBmYWxzZSwgZmFsc2UsIDAsICZvZmZzZXRzOjpmdW5jdGlv
+bnM6OlJpb3RfX1JlbmRlcmVyX19NYXRlcmlhbFJlZ2lzdHJ5X19HZXRTaW5n
+bGV0b25QdHIKCQl9LAoJCXsKCQkJewoJCQkJIkU4ID8gPyA/ID8gMEYgNTcg
+REIgNEMgOEIgQzAgRjMgMEYgNUEgREUiCgkJCX0sIHRydWUsIGZhbHNlLCBm
+YWxzZSwgMCwgJm9mZnNldHM6OmZ1bmN0aW9uczo6dHJhbnNsYXRlU3RyaW5n
+X1VOU0FGRV9ET05PVFVTRQoJCX0sCgkJewoJCQl7CgkJCQkiRTggPyA/ID8g
+PyA0QyAzQiBGOCAwRiA5NCBDMCIKCQkJfSwgdHJ1ZSwgZmFsc2UsIGZhbHNl
+LCAwLCAmb2Zmc2V0czo6ZnVuY3Rpb25zOjpHZXRHb2xkUmVkaXJlY3RUYXJn
+ZXQKCQl9Cgl9Owp9Owo=
