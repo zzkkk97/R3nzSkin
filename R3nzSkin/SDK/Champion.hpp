ad062d297c55c5f4dd1870e29602e54185daa0a8
KK <138672245+zzkkk97@users.noreply.github.com>
2024-02-28 13:04:05 +0800

    Update Champion.hpp

--- R3nzSkin/SDK/Champion.hpp
+++ R3nzSkin/SDK/Champion.hpp
@@ -1,13 +1,13 @@
-#pragma once
+#杂注一次
 
-#include "AString.hpp"
-#include "Pad.hpp"
-#include "Skin.hpp"
-#include "RiotArray.hpp"
+#包括 “收束。hpp“
+#包括 “Pad.hpp“
+#包括 “Skin.hpp“
+#包括 “RiotArray.hpp“
 
-class Champion {
-	PAD(0x8)
-	AString champion_name;
-	PAD(0xA0)
-	RiotArray<Skin> skins;
+班级冠军{
+衬垫(0x8)
+收敛冠军姓名;
+衬垫(0xA0)
+暴动阵列<皮肤> 外皮;
 };
